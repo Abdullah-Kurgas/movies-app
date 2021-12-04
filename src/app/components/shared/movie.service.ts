dcae31a808b9bd0f43d0577166d1eda9453684bb
Abdullah-Kurgas <abdullahkurgas07@example.com>
2021-12-04 22:39:46 +0100

    fixing service

--- src/app/components/shared/movie.service.ts
+++ src/app/components/shared/movie.service.ts
@@ -13,36 +13,36 @@ export class MovieService {
 
   /* Getting for home page */
   getPopularMovie(page: number) {
-    return this.http.get(environment.serverUrl + `/3/movie/popular?api_key=9b140afc6eba9d789da443808fbbc6fb&language=en-US&page=${page}`);
+    return this.http.get(`https://api.themoviedb.org/3/movie/popular?api_key=9b140afc6eba9d789da443808fbbc6fb&language=en-US&page=${page}`);
   }
   getTopRatedMovie(page: number) {
-    return this.http.get(environment.serverUrl + `/3/movie/top_rated?api_key=9b140afc6eba9d789da443808fbbc6fb&language=en-US&page=${page}`);
+    return this.http.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=9b140afc6eba9d789da443808fbbc6fb&language=en-US&page=${page}`);
   }
   getUpcomingMovie(page: number) {
-    return this.http.get(environment.serverUrl + `/3/movie/upcoming?api_key=9b140afc6eba9d789da443808fbbc6fb&language=en-US&page=${page}`);
+    return this.http.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=9b140afc6eba9d789da443808fbbc6fb&language=en-US&page=${page}`);
   }
 
 
   /* Geting for movie detail */
   getMovieById(id: number) {
-    return this.http.get(environment.serverUrl + `/3/movie/${id}?api_key=9b140afc6eba9d789da443808fbbc6fb&language=en-US&append_to_response=credits`);
+    return this.http.get(`https://api.themoviedb.org/3/movie/${id}?api_key=9b140afc6eba9d789da443808fbbc6fb&language=en-US&append_to_response=credits`);
   }
   getRecommended(id: number) {
-    return this.http.get(environment.serverUrl + `/3/movie/${id}/recommendations?api_key=9b140afc6eba9d789da443808fbbc6fb&language=en-US&page=1`);
+    return this.http.get(`https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=9b140afc6eba9d789da443808fbbc6fb&language=en-US&page=1`);
   }
   getSimilar(id: number) {
-    return this.http.get(environment.serverUrl + `/3/movie/${id}/similar?api_key=9b140afc6eba9d789da443808fbbc6fb&language=en-US&page=1`);
+    return this.http.get(`https://api.themoviedb.org/3/movie/${id}/similar?api_key=9b140afc6eba9d789da443808fbbc6fb&language=en-US&page=1`);
   }
 
   /* Getting for movie card */
   getGenre() {
-    return this.http.get(environment.serverUrl + "/3/genre/movie/list?api_key=9b140afc6eba9d789da443808fbbc6fb&language=en-US");
+    return this.http.get("https://api.themoviedb.org/3/genre/movie/list?api_key=9b140afc6eba9d789da443808fbbc6fb&language=en-US");
   }
 
   /* Searching for movie & showing in searched page */
 
   searchMovie(name: string) {
-    return this.http.get(environment.serverUrl + `/3/search/movie?api_key=9b140afc6eba9d789da443808fbbc6fb&language=en-US&query=${name}&page=1`)
+    return this.http.get(`https://api.themoviedb.org/3/search/movie?api_key=9b140afc6eba9d789da443808fbbc6fb&language=en-US&query=${name}&page=1`)
   }
 
 }
