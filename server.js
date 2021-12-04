cad3571cc0d32b8c2892c693de7a073eb8f9ada9
Abdullah-Kurgas <abdullahkurgas07@example.com>
2021-12-04 22:35:21 +0100

    second change in server js

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 //Install express server
+
+
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/movie-project'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/movie-project/' }),
-);
-
-// Start the app by listening on the default Heroku port
+app.use(express.static(__dirname + '/dist/movie-project'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname +
+        '/dist/movie-project/index.html'));
+});
 app.listen(process.env.PORT || 8080);
