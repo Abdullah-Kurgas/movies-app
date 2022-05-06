a9bc918f427e78f83f0d53f1ef9712b5d58bc803
abdullah.kurgas <abdullah.kurgas@morescreens.net>
2022-05-06 17:54:02 +0200

    Change search style on phones

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -12,12 +12,13 @@ export class MenuComponent implements OnInit {
 
   searchName: string = "";
   searchedMovies: any;
-  shown: boolean = false;
   imageUrl: string = "https://image.tmdb.org/t/p/w500";
   genres: any;
   visited: any = [];
 
+  shown: boolean = false;
   isHiddenSideBar: boolean = true;
+  searchType: string = 'normal';
 
   constructor(
     private movieService: MovieService,
@@ -31,20 +32,23 @@ export class MenuComponent implements OnInit {
 
   searchMovie() {
     if (this.searchName == "") return;
+
     this.router.navigate(["/searched"], { queryParams: { name: this.searchName } });
+    this.isHiddenSideBar = true;
+    this.shown = false;
   }
 
-  sensSearch(e: any) {
-    if (e.keyCode == 13) {
-      this.shown = false;
-      return;
-    };
-    if (this.searchName == "") {
-      this.shown = false;
-      return;
-    };
-    this.spinnerService.showLoading();
+  sensSearch(word: string, type: string) {
+    this.searchType = type;
 
+    if (word[word.length - 1] == ' ') return;
+    if (word.length == 0) {
+      this.shown = false;
+      return;
+    }
+
+
+    this.spinnerService.showLoading();
     this.movieService.searchMovie(this.searchName).subscribe(
       movies => {
         this.searchedMovies = movies;
@@ -58,7 +62,7 @@ export class MenuComponent implements OnInit {
     this.isHiddenSideBar = true;
 
     this.router.navigateByUrl("/movie/" + movie.id);
-    
+
     this.visited = JSON.parse(localStorage.getItem("visited") || "[]");
     this.visited.push(movie);
     localStorage.setItem("visited", JSON.stringify(this.visited));
@@ -75,7 +79,7 @@ export class MenuComponent implements OnInit {
     )
   }
 
-  showSideBar(){
+  showSideBar() {
     this.isHiddenSideBar = !this.isHiddenSideBar;
   }
 
